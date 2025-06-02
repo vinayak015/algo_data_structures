d580aeb3e052ffd957fd64ddb7a0abed360a8074
vinayak-sharan <103429405+vinayak-sharan@users.noreply.github.com>
2025-06-02 08:35:18 +0200

    heaps: LeetCode- Q295, heap question for computing median in a data stream.

---
+++ heaps/media_from_data_stream.py
@@ -0,0 +1,49 @@
+"""
+Q295
+The median is the middle value in an ordered integer list. If the size of the list is even, there is no middle value, and the median is the mean of the two middle values.
+
+For example, for arr = [2,3,4], the median is 3.
+For example, for arr = [2,3], the median is (2 + 3) / 2 = 2.5.
+Implement the MedianFinder class:
+
+MedianFinder() initializes the MedianFinder object.
+void addNum(int num) adds the integer num from the data stream to the data structure.
+double findMedian() returns the median of all elements so far. Answers within 10-5 of the actual answer will be accepted.
+"""
+
+import heapq
+
+
+class MedianFinder:
+
+    def __init__(self):
+        self.min_heap = []
+        self.max_heap = []
+
+    def addNum(self, num: int) -> None:
+        heapq.heappush(self.max_heap, -num)
+        heapq.heappush(self.min_heap, -heapq.heappop(self.max_heap))
+        if len(self.min_heap) > len(self.max_heap):
+            heapq.heappush(self.max_heap, -heapq.heappop(self.min_heap))
+
+    def findMedian(self) -> float:
+        if len(self.max_heap) > len(self.min_heap):
+            return -self.max_heap[0]
+        return (-self.max_heap[0] + self.min_heap[0]) / 2
+
+
+if __name__ == "__main__":
+    arr = [1, 3, 7, 13, 36, 100]
+    m = MedianFinder()
+    for ar in arr:
+        m.addNum(ar)
+    print(m.findMedian())
+    m.addNum(50)
+    print(m.findMedian())
+
+"""
+Time Complexity:
+addNum takes O(log(n)) for pushing and popping from heap 
+findNum takes O(1)
+Therefore, overall complexity is O(log(n)) for adding elements and O(1) for finding out the mean.
+"""
